1c93b47b93b2bf3184b7a70eedb77bc9bcbe54e3
John Carlo Asilo <jcarloasilo@gmail.com>
2024-01-08 20:56:21 +0800

    update max registrants

--- src/lib/shared/config.ts
+++ src/lib/shared/config.ts
@@ -1,4 +1,4 @@
-export const MAX_LEFT_SIDE = 2;
-export const MAX_RIGHT_SIDE = 1000;
+export const MAX_LEFT_SIDE = 3500;
+export const MAX_RIGHT_SIDE = 2500;
 
 export const MAX_REGISTRANTS = MAX_LEFT_SIDE + MAX_RIGHT_SIDE;
