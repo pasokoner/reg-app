fc83bb519f4f3dc82f62df2e30882aca931c612e
John Carlo Asilo <jcarloasilo@gmail.com>
2024-03-22 08:33:30 +0800

    adjust result text position

--- src/routes/rememberingErlinda2PM/scanner/+page.svelte
+++ src/routes/rememberingErlinda2PM/scanner/+page.svelte
@@ -1,32 +1,32 @@
 <script lang="ts">
-	import { cameraSchema, type CameraSchema } from '$lib/zod-schemas';
+	import { cameraSchema, type CameraSchema } from "$lib/zod-schemas";
 
-	import { onMount, tick } from 'svelte';
-	import QrScanner from 'qr-scanner';
-	import { cn } from '$lib/utils';
-	import { LoaderIcon } from 'lucide-svelte';
+	import { onMount, tick } from "svelte";
+	import QrScanner from "qr-scanner";
+	import { cn } from "$lib/utils";
+	import { LoaderIcon } from "lucide-svelte";
 
-	import * as Form from '$lib/components/ui/form';
-	import { type SuperValidated, type Infer, superForm } from 'sveltekit-superforms';
-	import { zodClient } from 'sveltekit-superforms/adapters';
-	import Input from '$lib/components/ui/input/input.svelte';
+	import * as Form from "$lib/components/ui/form";
+	import { type SuperValidated, type Infer, superForm } from "sveltekit-superforms";
+	import { zodClient } from "sveltekit-superforms/adapters";
+	import Input from "$lib/components/ui/input/input.svelte";
 
 	export let data: SuperValidated<Infer<CameraSchema>>;
 
 	let scannedResult: string;
 	let videoFeed: HTMLVideoElement;
 	let submitBtn: HTMLButtonElement;
-	let successMessage = '';
+	let successMessage = "";
 
 	const form = superForm(data, {
-		dataType: 'json',
+		dataType: "json",
 		validators: zodClient(cameraSchema),
 		onUpdate(event) {
 			if (event.form.valid) {
-				successMessage = 'Scanned successfully!';
+				successMessage = "Scanned successfully!";
 				return;
 			}
-			successMessage = '';
+			successMessage = "";
 		}
 	});
 
@@ -66,11 +66,16 @@
 </script>
 
 <main class="flex h-screen max-h-screen w-full flex-col">
-	<div class="flex h-16 items-center justify-center bg-blue text-xl font-semibold">SCANNER</div>
+	<div
+		class="absolute top-0 flex h-16 w-full items-center justify-center bg-black text-sm font-semibold text-white sm:text-xl"
+	>
+		REMEMBERING ERLINDA 2PM SCANNER
+	</div>
+
 	<!-- svelte-ignore a11y-media-has-caption -->
 	<video bind:this={videoFeed} class="flex-1" />
 
-	<form method="POST" use:enhance class="max-w-[600px] mx-auto space-y-4">
+	<form method="POST" use:enhance class="absolute bottom-0 w-full">
 		<Form.Field {form} name="registrantId" class="hidden">
 			<Form.Control let:attrs>
 				<Form.Label>Full Name</Form.Label>
@@ -82,19 +87,23 @@
 		<Form.Errors errors={$errors._errors} />
 
 		{#if $submitting}
-			<div class="flex h-16 items-center justify-center gap-x-2 bg-orange-dark text-xl">
+			<div class="flex h-16 items-center justify-center gap-x-2 bg-orange-700 text-xl">
 				<LoaderIcon class="animate-spin" /> Please wait...
 			</div>
 		{:else if successMessage}
-			<div class={cn('flex h-16 items-center justify-center bg-green-dark text-xl')}>
+			<div class="flex h-16 items-center justify-center bg-green-700 text-xl">
 				{successMessage}
 			</div>
 		{:else if $errors._errors && $errors._errors.length > 0}
 			{#each $errors._errors as err}
-				<div class={cn('flex h-16 items-center justify-center bg-green-dark text-xl bg-red-800')}>
+				<div class="bg-green-dark flex h-16 items-center justify-center bg-red-800 text-xl">
 					{err}
 				</div>
 			{/each}
+		{:else}
+			<div class="flex h-16 w-full items-center justify-center bg-black text-xl text-white">
+				STANDBY
+			</div>
 		{/if}
 
 		<button type="submit" bind:this={submitBtn} class="hidden" />
